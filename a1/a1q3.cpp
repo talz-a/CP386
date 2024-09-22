54543c4cc0788412825afeb2eb4359a5bb5df567
ta-02 <talal.gl2004@gmail.com>
2024-09-22 15:08:11 -0400

    finished a1

--- a1/a1q3.cpp
+++ a1/a1q3.cpp
@@ -6,36 +6,71 @@
 using namespace std;
 
 int main() {
-  int fd[2];
-  const string cmds[3] = {"ps axo sess", "sort -u ", "wc -l"};
+  int pipe1[2], pipe2[2];
 
-  if (pipe(fd) == -1) {
-    cerr << "Pipe creation failed!" << std::endl;
+  if (pipe(pipe1) == -1) {
+    cerr << "Pipe1 creation failed!" << endl;
     return 1;
   }
 
-  pid_t pid = fork();
-
-  if (pid == -1) {
-    cerr << "Fork failed!" << std::endl;
+  pid_t pid1 = fork();
+  if (pid1 == -1) {
+    cerr << "Fork for child 1 failed!" << endl;
     return 1;
   }
 
-  if (pid == 0) {
-    close(fd[1]);
+  if (pid1 == 0) {
+    close(pipe1[0]);
+
+    close(1);
+    dup(pipe1[1]);
+    close(pipe1[1]);
+
+    execlp("ps", "ps", "axo", "sess", NULL);
+    cerr << "Exec for ps failed!" << endl;
+    return 1;
+  }
+
+  if (pipe(pipe2) == -1) {
+    cerr << "Pipe2 creation failed!" << endl;
+    return 1;
+  }
+
+  pid_t pid2 = fork();
+  if (pid2 == -1) {
+    cerr << "Fork for child 2 failed!" << endl;
+    return 1;
+  }
+
+  if (pid2 == 0) {
+    close(pipe1[1]);
 
     close(0);
-    dup(fd[0]);
+    dup(pipe1[0]);
 
-    close(fd[0]);
-    execlp("wc", "wc", NULL);
+    close(pipe1[0]);
 
-    cerr << "Exec failed!" << std::endl;
-  } else {
-    close(fd[0]);
-    // do the things in here in parent
-    close(fd[1]);
+    close(pipe2[0]);
+    close(1);
+    dup(pipe2[1]);
+
+    close(pipe2[1]);
+
+    execlp("sort", "sort", "-u", NULL);
+    cerr << "Exec for sort failed!" << endl;
+    return 1;
   }
 
-  return 0;
+  close(pipe1[0]);
+  close(pipe1[1]);
+  close(pipe2[1]);
+
+  close(0);
+  dup(pipe2[0]);
+
+  close(pipe2[0]);
+
+  execlp("wc", "wc", "-l", NULL);
+  cerr << "Exec for wc failed!" << endl;
+  return 1;
 }
