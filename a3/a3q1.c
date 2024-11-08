e76188ba68a5ea2249e9d680c0b6fae3517285ed
ta-02 <talal.gl2004@gmail.com>
2024-11-08 18:26:15 -0500

    updated code and removed comments

--- a3/a3q1.c
+++ a3/a3q1.c
@@ -2,67 +2,56 @@
 #include <semaphore.h>
 #include <stdio.h>
 
-#define MAX 5 // Buffer size
+#define MAX 5
 
-sem_t count; // Semaphore to keep track of the number of items in the buffer
-pthread_mutex_t mutex; // Mutex lock for buffer access
+sem_t count;
+pthread_mutex_t mutex;
 
 int buffer[MAX];
-int fill = 0; // Index for producer to put data
-int use = 0;  // Index for consumer to get data
-int start_consuming =
-    0; // Flag indicating when the consumer should start (0 = false, 1 = true)
+int fill = 0;
+int use = 0;
+int start_consuming = 0;
 
 void *producer(void *arg) {
   for (int i = 0; i < 10; i++) {
     pthread_mutex_lock(&mutex);
 
-    // Wait if buffer is full
     while ((fill + 1) % MAX == use) {
       pthread_mutex_unlock(&mutex);
-      // Busy wait
       pthread_mutex_lock(&mutex);
     }
 
-    // Put item into buffer
     buffer[fill] = i;
     fill = (fill + 1) % MAX;
 
-    // Set start_consuming to true after producing 3 items
-    if (i == 2) { // After producing the third item (indices 0,1,2)
+    if (i == 2) {
       start_consuming = 1;
     }
 
     printf("Produced %d\n", i);
-
-    sem_post(&count); // Signal that a new item is available
-
+    sem_post(&count);
     pthread_mutex_unlock(&mutex);
   }
   return NULL;
 }
 
 void *consumer(void *arg) {
-  // Wait until at least 3 items have been produced
   pthread_mutex_lock(&mutex);
+
   while (!start_consuming) {
     pthread_mutex_unlock(&mutex);
-    // Busy wait
     pthread_mutex_lock(&mutex);
   }
   pthread_mutex_unlock(&mutex);
 
   for (int i = 0; i < 10; i++) {
-    sem_wait(&count); // Wait for an item to be available
-
+    sem_wait(&count);
     pthread_mutex_lock(&mutex);
 
-    // Get item from buffer
     int val = buffer[use];
     use = (use + 1) % MAX;
 
     printf("Consumed %d\n", val);
-
     pthread_mutex_unlock(&mutex);
   }
   return NULL;
@@ -71,22 +60,17 @@ void *consumer(void *arg) {
 int main() {
   pthread_t prod_thread, cons_thread;
 
-  // Initialize semaphore and mutex
-  sem_init(&count, 0, 0); // Buffer initially empty
+  sem_init(&count, 0, 0);
   pthread_mutex_init(&mutex, NULL);
 
-  // Create producer and consumer threads
   pthread_create(&prod_thread, NULL, producer, NULL);
   pthread_create(&cons_thread, NULL, consumer, NULL);
 
-  // Wait for threads to finish
   pthread_join(prod_thread, NULL);
   pthread_join(cons_thread, NULL);
 
-  // Destroy semaphore and mutex
   sem_destroy(&count);
   pthread_mutex_destroy(&mutex);
 
   return 0;
 }
-
--- a3/a3q2.c
+++ a3/a3q2.c
@@ -1,157 +1,120 @@
+#include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <pthread.h>
-#include <unistd.h>
 #include <time.h>
+#include <unistd.h>
 
 #define MAX_WAITING_CUSTOMERS 5
 #define NUM_CUSTOMERS 10
 
-// Mutex and condition variables for synchronization
 pthread_mutex_t mutex;
 pthread_cond_t barber_cond;
 pthread_cond_t customer_cond;
 
-// Variables to track the state
-int waiting_customers = 0;        // Number of customers waiting
-int customers_remaining = NUM_CUSTOMERS; // Number of customers left to process
-int barber_sleeping = 0;          // Barber's sleeping status
+int waiting_customers = 0;
+int customers_remaining = NUM_CUSTOMERS;
+int barber_sleeping = 0;
 
-// Circular queue to represent waiting room chairs
 int waiting_room[MAX_WAITING_CUSTOMERS];
 int front = 0, rear = 0;
 
 void *barber(void *arg) {
-    while (1) {
-        pthread_mutex_lock(&mutex);
+  while (1) {
+    pthread_mutex_lock(&mutex);
 
-        // If there are no customers, barber goes to sleep
-        while (waiting_customers == 0) {
-            if (customers_remaining == 0) {
-                // No more customers will arrive
-                printf("Barber is closing shop.\n");
-                pthread_mutex_unlock(&mutex);
-                return NULL;
-            }
-
-            barber_sleeping = 1;
-            printf("Barber is sleeping.\n");
-            pthread_cond_wait(&barber_cond, &mutex);
-            barber_sleeping = 0;
-        }
-
-        // Serve the next customer
-        int customer_id = waiting_room[front];
-        front = (front + 1) % MAX_WAITING_CUSTOMERS;
-        waiting_customers--;
-
-        printf("Barber is cutting hair of customer %d.\n", customer_id);
-
-        // Signal the customer that the barber is ready
-        pthread_cond_signal(&customer_cond);
-
+    while (waiting_customers == 0) {
+      if (customers_remaining == 0) {
         pthread_mutex_unlock(&mutex);
+        return NULL;
+      }
 
-        // Simulate hair cutting
-        sleep(2);
-
-        printf("Barber finished cutting hair of customer %d.\n", customer_id);
-
-        // Customer will decrement customers_remaining after haircut
+      barber_sleeping = 1;
+      printf("Barber is going to sleep\n");
+      pthread_cond_wait(&barber_cond, &mutex);
+      barber_sleeping = 0;
     }
-    return NULL;
+
+    int customer_id = waiting_room[front];
+    front = (front + 1) % MAX_WAITING_CUSTOMERS;
+    waiting_customers--;
+
+    printf("Barber is cutting hair\n");
+
+    pthread_cond_signal(&customer_cond);
+    pthread_mutex_unlock(&mutex);
+
+    sleep(2);
+  }
+  return NULL;
 }
 
 void *customer(void *arg) {
-    int customer_id = *(int *)arg;
-    free(arg); // Free the allocated memory immediately
+  int customer_id = *(int *)arg;
+  free(arg);
 
-    pthread_mutex_lock(&mutex);
+  pthread_mutex_lock(&mutex);
 
-    if (waiting_customers < MAX_WAITING_CUSTOMERS) {
-        // Add customer to the waiting room
-        waiting_room[rear] = customer_id;
-        rear = (rear + 1) % MAX_WAITING_CUSTOMERS;
-        waiting_customers++;
+  if (waiting_customers < MAX_WAITING_CUSTOMERS) {
+    waiting_room[rear] = customer_id;
+    rear = (rear + 1) % MAX_WAITING_CUSTOMERS;
+    waiting_customers++;
 
-        printf("Customer %d is waiting. Waiting customers: %d\n", customer_id, waiting_customers);
+    printf("Customer %d is waiting\n", customer_id);
 
-        // Wake up the barber if he is sleeping
-        if (barber_sleeping) {
-            printf("Customer %d wakes up the barber.\n", customer_id);
-            pthread_cond_signal(&barber_cond);
-        }
-
-        // Wait until the barber is ready
-        pthread_cond_wait(&customer_cond, &mutex);
-
-        printf("Customer %d is getting a haircut.\n", customer_id);
-
-        pthread_mutex_unlock(&mutex);
-
-        // Simulate hair cutting (already simulated in barber thread)
-        sleep(2);
-
-        // After haircut is done
-        pthread_mutex_lock(&mutex);
-        customers_remaining--;
-        // Signal the barber in case he's waiting to check for remaining customers
-        pthread_cond_signal(&barber_cond);
-        pthread_mutex_unlock(&mutex);
-
-    } else {
-        // No chairs available; customer leaves
-        printf("No available chairs. Customer %d leaves.\n", customer_id);
-
-        customers_remaining--;
-
-        // Signal the barber in case he's waiting to check for remaining customers
-        pthread_cond_signal(&barber_cond);
-
-        pthread_mutex_unlock(&mutex);
+    if (barber_sleeping) {
+      pthread_cond_signal(&barber_cond);
     }
 
-    return NULL;
+    pthread_cond_wait(&customer_cond, &mutex);
+
+    printf("Customer %d is getting a haircut\n", customer_id);
+
+    pthread_mutex_unlock(&mutex);
+
+    sleep(2);
+
+    pthread_mutex_lock(&mutex);
+    customers_remaining--;
+    pthread_cond_signal(&barber_cond);
+    pthread_mutex_unlock(&mutex);
+
+  } else {
+    customers_remaining--;
+    pthread_cond_signal(&barber_cond);
+    pthread_mutex_unlock(&mutex);
+  }
+
+  return NULL;
 }
 
 int main() {
-    pthread_t barber_thread;
-    pthread_t customer_threads[NUM_CUSTOMERS];
+  pthread_t barber_thread;
+  pthread_t customer_threads[NUM_CUSTOMERS];
 
-    // Initialize mutex and condition variables
-    pthread_mutex_init(&mutex, NULL);
-    pthread_cond_init(&barber_cond, NULL);
-    pthread_cond_init(&customer_cond, NULL);
+  pthread_mutex_init(&mutex, NULL);
+  pthread_cond_init(&barber_cond, NULL);
+  pthread_cond_init(&customer_cond, NULL);
 
-    // Seed the random number generator
-    srand(time(NULL));
+  srand(time(NULL));
 
-    // Create barber thread
-    pthread_create(&barber_thread, NULL, barber, NULL);
+  pthread_create(&barber_thread, NULL, barber, NULL);
 
-    // Create customer threads
-    for (int i = 0; i < NUM_CUSTOMERS; i++) {
-        // Simulate random arrival times
-        sleep(rand() % 3);
+  for (int i = 0; i < NUM_CUSTOMERS; i++) {
+    sleep(rand() % 3);
+    int *customer_id = malloc(sizeof(int));
+    *customer_id = i;
+    pthread_create(&customer_threads[i], NULL, customer, customer_id);
+  }
 
-        int *customer_id = malloc(sizeof(int));
-        *customer_id = i + 1;
-        pthread_create(&customer_threads[i], NULL, customer, customer_id);
-    }
+  for (int i = 0; i < NUM_CUSTOMERS; i++) {
+    pthread_join(customer_threads[i], NULL);
+  }
 
-    // Wait for all customer threads to finish
-    for (int i = 0; i < NUM_CUSTOMERS; i++) {
-        pthread_join(customer_threads[i], NULL);
-    }
+  pthread_join(barber_thread, NULL);
 
-    // Wait for the barber thread to finish
-    pthread_join(barber_thread, NULL);
+  pthread_mutex_destroy(&mutex);
+  pthread_cond_destroy(&barber_cond);
+  pthread_cond_destroy(&customer_cond);
 
-    // Cleanup
-    pthread_mutex_destroy(&mutex);
-    pthread_cond_destroy(&barber_cond);
-    pthread_cond_destroy(&customer_cond);
-
-    printf("All customers have been served or left. Barber shop is closing.\n");
-    return 0;
+  return 0;
 }
